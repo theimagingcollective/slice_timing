a6893fed9b470ee365fe502ec0005c715b4e47ef
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2017-08-20 15:14:11 -0400

    Renamed: SlicesInfo to SlicesParams

--- tests/test_process_input.py
+++ tests/test_process_input.py
@@ -3,16 +3,16 @@
 from collections import namedtuple
 
 from src.preprocessing import process_inputs
-from src.preprocessing import SlicesInfo
+from src.preprocessing import SlicesParams
 
 
 # assertRaise
 
 
-def __generate_test_data():
+def _generate_test_data():
     SliceTimesInput = namedtuple('SliceTimesInput', 'repetitiontime num_slices ordering')
     test_data = {
-        SliceTimesInput(repetitiontime=3, num_slices=48, ordering='interleaved'): SlicesInfo(
+        SliceTimesInput(repetitiontime=3, num_slices=48, ordering='interleaved'): SlicesParams(
                     repetitiontime=3, num_slices=48,
                     scan_order=[0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34,
                                 36, 38,
@@ -20,13 +20,13 @@ def __generate_test_data():
                                 29, 31,
                                 33, 35, 37, 39, 41, 43, 45, 47]),
         
-        SliceTimesInput(repetitiontime=3, num_slices=48, ordering='straight'): SlicesInfo(
+        SliceTimesInput(repetitiontime=3, num_slices=48, ordering='straight'): SlicesParams(
                     repetitiontime=3, num_slices=48,
                     scan_order=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
                                 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
                                 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47]),
         
-        SliceTimesInput(repetitiontime=10, num_slices=100, ordering='interleaved'): SlicesInfo(
+        SliceTimesInput(repetitiontime=10, num_slices=100, ordering='interleaved'): SlicesParams(
                     repetitiontime=10, num_slices=100,
                     scan_order=[0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34,
                                 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68,
@@ -35,7 +35,7 @@ def __generate_test_data():
                                 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73,
                                 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99]),
         
-        SliceTimesInput(repetitiontime=0, num_slices=48, ordering='straight'): SlicesInfo(
+        SliceTimesInput(repetitiontime=0, num_slices=48, ordering='straight'): SlicesParams(
                     repetitiontime=0, num_slices=48,
                     scan_order=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
                                 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
@@ -46,7 +46,7 @@ def __generate_test_data():
 
 
 def test_process_inputs():
-    test_data = __generate_test_data()
+    test_data = _generate_test_data()
     for test_input, expected in test_data.items():
         actual = process_inputs(*test_input)
         # assert actual == expected
